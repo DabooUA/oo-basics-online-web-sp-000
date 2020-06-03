2c8afbf3a8385c1be3398bbadcf7a17a1d7a3bff
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-03 15:36:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
 
-  attr_accessor :color, :size, :material, 
+  attr_accessor :color, :size, :material,
   attr_reader :brand, :condition
 
   def initialize(brand)
@@ -14,5 +14,5 @@ class Shoe
   def condition=(condition = "new")
     @condition = condition
   end
-    
+
 end
