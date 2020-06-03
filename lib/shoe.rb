7f3d1e6597e62ea1f32e6fc1ba24f965db8f5bd7
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-03 15:40:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,8 @@
 class Shoe
 
   attr_accessor :color, :size, :material,
-  attr_reader :brand, :condition
+  attr_reader :brand
+  attr_reader :condition
 
   def initialize(brand)
     @brand = brand
